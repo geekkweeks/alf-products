b25cf4211ce4d9f04407596c65cce0d61c4c4cb9
alfan <zahgreat30@gmail.com>
2024-01-09 14:50:01 +0700

    unit test - successful scenario for user test

---
+++ test/user.test.js
@@ -0,0 +1,26 @@
+import supertest from "supertest";
+import { web } from "../src/application/web.js";
+import { prismaClient } from "../src/application/database.js";
+
+describe("POST /api/users", function () {
+  const usernameTest = "alfanTest";
+  const nameTest = "alfantest test";
+  afterEach(async () => {
+    await prismaClient.user.deleteMany({
+      where: {
+        username: usernameTest,
+      },
+    });
+  });
+  it("should be registered for the new user", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: usernameTest,
+      password: "XCAS",
+      name: nameTest,
+    });
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.username).toBe(usernameTest);
+    expect(result.body.data.password).toBeUndefined();
+  });
+});
